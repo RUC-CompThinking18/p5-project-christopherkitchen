afdcb425e124465e63d7dfe7935674e7444eae8a
Christopher Kitchen <cbk42@scarletmail.rutgers.edu>
2018-12-04 16:41:00 -0500

    Push 1
    tried uploading a photo, but my browser won't display it

--- sketch.js
+++ sketch.js
@@ -1,6 +1,11 @@
-function setup() {
-  // put setup code here
+var img;
+function preload() {
+  img = loadImage('raindrops1.jpg');
 }
+function setup() {
+  image(img, 0, 0);
+}
+
 
 function draw() {
   // put drawing code here

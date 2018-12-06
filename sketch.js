d444a1e4797625d5e69ba575818dde9256d906a8
Christopher Kitchen <cbk42@scarletmail.rutgers.edu>
2018-12-06 16:47:59 -0500

    12/6 update
    need to add get() function

--- sketch.js
+++ sketch.js
@@ -1,12 +1,26 @@
 var img;
-function preload() {
-  img = loadImage('raindrops1.jpg');
+function preload() { //image function needs this
+  img = loadImage('raindrops-1.jpg'); //image must be less than 1000x1000
 }
 function setup() {
+  createCanvas(1000, 1000) //image size matches canvas
   image(img, 0, 0);
 }
 
-
 function draw() {
-  // put drawing code here
+
+}
+
+var value = 0;
+function draw() {
+  fill(value);
+  rect(25, 25, 50, 50);
+}
+
+function mousePressed() {
+  if (value === 0) {
+    value = 255;
+  } else {
+    value = 0;
+  }
 }
